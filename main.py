790387053d6b32a7b17c4ad1d706724225be9c19
abdulkadirbatibay <abdulkadirbatibay@gmail.com>
2025-10-15 20:20:35 +0200

    encode and decode functions are working.

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+want_to_continue = True
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
 
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    shift_amount %= len(alphabet)
 
+    if encode_or_decode == "decode":
+        shift_amount = -shift_amount
 
-def encrypt(original_text, shift_amount):
-    encrypted_text = ""
     for letter in original_text:
-        original_index = alphabet.index(letter)
-        new_index = (original_index + shift_amount) % len(alphabet) # index will always be in the range of 0 and 25
-        encrypted_text += alphabet[new_index]
+        if letter in alphabet:
+            original_index = alphabet.index(letter)
+            new_index = (original_index + shift_amount)
+            output_text += alphabet[new_index]
+        else:
+            output_text += letter
 
-    print(f"Here is the encoded result: {encrypted_text}")
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-if direction == "encode":
-    encrypt(original_text = text, shift_amount = shift)
+while want_to_continue :
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    answer = input("To continue type \"yes\" and to stop type \"no\" :")
+    if answer == "no":
+        want_to_continue = False
+        print("Goodbye")
 
